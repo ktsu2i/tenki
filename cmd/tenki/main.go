343897318f5bd4da2acb9bdb786834dc87f93c21
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2026-06-07 14:02:37 +0900

    Prepare go install distribution (#3)

--- cmd/tenki/main.go
+++ cmd/tenki/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/ktsu2i/tenki/internal/cli"
 )
 
-var version = "dev"
-
 func main() {
-	os.Exit(cli.Main(os.Args[1:], os.Stdout, os.Stderr, version))
+	os.Exit(cli.Main(os.Args[1:], os.Stdout, os.Stderr, version()))
+}
+
+func version() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "dev"
+	}
+	if info.Main.Version == "" || info.Main.Version == "(devel)" {
+		return "dev"
+	}
+	return info.Main.Version
 }
